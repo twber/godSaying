5e236289b33c000e27d4dd601325cc1761a52646
twber <hctsai@umich.edu>
2020-08-31 01:13:22 +0800

    Initial Commit

---
+++ godSaying/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  godSaying
+//
+//  Created by Bernice TSAI on 2020/8/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
